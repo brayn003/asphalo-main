2a010692360920be155e49a80fb05c41b898fb60
Rudraprasad Das <brayn003@gmail.com>
2016-10-18 03:45:26 +0530

    added buggy sidebar

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -12,11 +12,27 @@ function sidebarScroll() {
 	// console.log($('#scroll-nav').children('.active').index())
 	// console.log($('#scroll-nav').children('li:not(.active)').outerHeight())
 	// console.log($('#scroll-nav').children('li.active').outerHeight())
-	// console.log('correntoffset',currentOffset)
-	// console.log('newoffset',newOffset)
+	console.log('correntoffset',currentOffset)
+	console.log('newoffset',newOffset)
 	// console.log(newOffset - currentOffset)
 	// console.log( newOffset - ((activeHeight/2)-currentOffset))
 
 	$('#scroll-nav').css('transform','translate3d(0,'+(newOffset - currentOffset)+'px,0)');
 }
 sidebarScroll()
+
+function toggleActiveClass(){
+	$('#scroll-nav').children('li').each(function(){
+		$(this).click(function(){
+			$('#scroll-nav').children('li').each(function(){
+				$(this).removeClass('active');
+			})
+			$(this).addClass('active');
+			// setTimeout(function(){
+				sidebarScroll();
+			// },2000)
+		})
+	})
+}
+
+toggleActiveClass()
